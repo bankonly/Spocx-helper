24dfb0a8e4c90599ea333cd4f8736dabbac5454b
bankonly <for21bn01@gmail.com>
2021-03-09 16:01:43 +0700

    first commit

--- helpers/video-upload.js
+++ helpers/video-upload.js
@@ -1,3 +1,6 @@
 const ffmpeg = require("fluent-ffmpeg");
 
-ffmpeg().input("../files/exmaple-video.mp4").size("426x240").save("../files/exmaple-video-426x240.mp4")
+const PATH = "/root/Spocx/video/video";
+const filename = "0ca791e2-5075-4686-b10e-ad6cacdade9b1614519194361.mp4";
+
+ffmpeg().input(`${PATH}/${filename}`).size("426x240").save(`${PATH}/426x240/${filename}`)
